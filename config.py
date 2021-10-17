3bcee1d17786767636c961149a5e0ca966f8f1d3
msroth <scott.roth.dctm@gmail.com>
2021-10-17 15:03:59 -0400

    initial upload

---
+++ config.py
@@ -0,0 +1,21 @@
+# list of city Ids
+# http://bulk.openweathermap.org/sample/
+# Manassas 20111
+weather_city = 20111
+# where to get weather key?
+weather_api_key = '2ec33ce45f88dfcf30961aaf6bc2a0b7'
+# number of minutes between weather updates
+weather_update_rate = 5
+# stock market symbols
+symbols=['AAPL', 'AMZN', 'GOOGL', 'MSFT', 'TSLA']
+# number of minutes between market updates
+market_update_rate = 5
+# number of seconds between time format changes
+face_flip_rate = 5
+# News API key
+news_api_key = 'a53cd133d3824cd5ae243cf7aeba2728'
+# number of minutes before headline updates
+news_update_rate = 10
+# news sources
+# https://newsapi.org/docs/endpoints/sources
+news_source = 'breitbart-news'
